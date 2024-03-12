d7f4da01b39da6856c687a185b4dc27b8780c7ee
AflatTaif <taifuryz5@gmail.com>
2024-03-12 13:03:36 +0600

    assignment one added

---
+++ assignmentOne.dart
@@ -0,0 +1,14 @@
+import 'assignmentOneClasses.dart';
+
+void main(){
+  // instance of Media
+
+  Media media= Media();
+  media.play();
+  // instance of Song
+  Song song= Song('Atif Aslam');
+  song.play();
+
+
+
+}
